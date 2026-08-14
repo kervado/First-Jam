4df233712b72254fe92a40b25047b7870ab42b0b
artem-gd <168022207+artem-gd@users.noreply.github.com>
2026-08-14 13:47:43 +0300

    Game process

---
+++ main/controller.go
@@ -0,0 +1,10 @@
+components {
+  id: "game"
+  component: "/main/game.script"
+}
+embedded_components {
+  id: "element_factory"
+  type: "factory"
+  data: "prototype: \"/main/element.go\"\n"
+  ""
+}
---
+++ main/element.go
@@ -0,0 +1,16 @@
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "default_animation: \"square\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/main/main.atlas\"\n"
+  "}\n"
+  "size {\n"
+  "  x: 64.0\n"
+  "  y: 64.0\n"
+  "}\n"
+  "size_mode: SIZE_MODE_MANUAL\n"
+  ""
+}
