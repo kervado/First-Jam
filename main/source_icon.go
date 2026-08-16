1b3017a19ef19db7eb975e7b57fd0105b163edd5
artem-gd <168022207+artem-gd@users.noreply.github.com>
2026-08-16 14:48:20 +0300

    New Icons

--- main/source_icon.go
+++ main/source_icon.go
@@ -1,12 +1,17 @@
 components {
   id: "script"
-  component: "/main/request_icon.script"
+  component: "/main/source_icon.script"
 }
 embedded_components {
   id: "sprite"
   type: "sprite"
-  data: "default_animation: \"icon_cat 1\"\n"
+  data: "default_animation: \"icon_space_cat 1\"\n"
   "material: \"/builtins/materials/sprite.material\"\n"
+  "size {\n"
+  "  x: 100.0\n"
+  "  y: 100.0\n"
+  "}\n"
+  "size_mode: SIZE_MODE_MANUAL\n"
   "textures {\n"
   "  sampler: \"texture_sampler\"\n"
   "  texture: \"/assets/icons/Icons.atlas\"\n"
